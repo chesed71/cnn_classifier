6aea00a854f1e308f500e8b9725a2e4d8329d914
chesed71 <chesed71@gmail.com>
2021-07-28 13:55:37 +0900

    모델 추가

--- cnn_classifier.py
+++ cnn_classifier.py
@@ -4,3 +4,11 @@ import tensorflow_datasets as tfdf
 
 dataset, info = tfds.load("imdb_reviews/subwords8k", with_info=True, as_supervised=True)
 train_dataset, test_dataset = dataset['train'], dataset['test']
+
+model = tf.keras.Sequencial([
+    tf.keras.layers.Embedding(10000, 64),
+    tf.keras.layers.Conv1D(128, 5, activation='relu'),
+    tf.keras.layers.GlobalAveragePooling1D(),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(1, activateion='sigmoid')
+])
